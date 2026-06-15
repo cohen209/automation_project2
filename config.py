ac173193455064ff76a1761079632bd22165b7c9
Yehonatan cohen <kohen4578@gmail.com>
2026-06-16 02:59:01 +0300

    admin login test + config

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+BASE_URL = os.environ.get('BASE')
+ADMIN_EMAIL = os.environ.get('ADMIN_EMAIL')
+ADMIN_PASSWORD = os.environ.get('ADMIN_PASSWORD')
--- test_simple_login.py
+++ test_simple_login.py
@@ -1,7 +1,7 @@
 def test_simple_login(page):
-    page.goto("https://practicetestautomation.com/practice-test-login/")
-    page.fill("#username", "student")
-    page.fill("#password", "Password123")
-    page.click("#submit")
+    page.goto('BASE')
+    page.fill("#username", 'ADMIN')
+    page.fill("#password", 'PASSWORD')
+    page.get_by_role("button", name="Login").click()
     assert page.locator(".post-title").text_content() == "Logged In Successfully"
-    assert page.locator("div.post-content strong").text_content() == "Congratulations student. You successfully logged in!"
+   
